2ef011f04a881a33454dd3fa056b6a98005e0686
giantbing <giantbing@hotmail.com>
2017-09-18 18:53:50 +0800

    修改设置全屏位置

--- giantbaselibrary/src/main/java/giantbing/zonlinks/com/giantbaselibrary/Util/ActivityUtil.java
+++ giantbaselibrary/src/main/java/giantbing/zonlinks/com/giantbaselibrary/Util/ActivityUtil.java
@@ -1,6 +1,8 @@
 package giantbing.zonlinks.com.giantbaselibrary.Util;
 
 import android.app.Activity;
+import android.os.Build;
+import android.view.View;
 import android.view.WindowManager;
 
 /**
@@ -18,5 +20,28 @@ public class ActivityUtil {
         mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         //透明导航栏
         mActivity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    }
+
+    public static void hideSystemNavigationBar(Activity activity) {
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            View view = activity.getWindow().getDecorView();
+            view.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = activity.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+    public static void showSystemNavigationBar(Activity activity) {
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            View view = activity.getWindow().getDecorView();
+            view.setSystemUiVisibility(View.VISIBLE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = activity.getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
     }
 }
--- giantbaselibrary/src/main/java/giantbing/zonlinks/com/giantbaselibrary/Util/DeviceUtil.java
+++ giantbaselibrary/src/main/java/giantbing/zonlinks/com/giantbaselibrary/Util/DeviceUtil.java
@@ -36,28 +36,7 @@ public class DeviceUtil  {
         am.set(AlarmManager.RTC_WAKEUP, 10000, pendingIntent);
 
     }
-    public static void hideSystemNavigationBar(Activity activity) {
-        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
-            View view = activity.getWindow().getDecorView();
-            view.setSystemUiVisibility(View.GONE);
-        } else if (Build.VERSION.SDK_INT >= 19) {
-            View decorView = activity.getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
-            decorView.setSystemUiVisibility(uiOptions);
-        }
-    }
-    public static void showSystemNavigationBar(Activity activity) {
-        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
-            View view = activity.getWindow().getDecorView();
-            view.setSystemUiVisibility(View.VISIBLE);
-        } else if (Build.VERSION.SDK_INT >= 19) {
-            View decorView = activity.getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
-            decorView.setSystemUiVisibility(uiOptions);
-        }
-    }
+
 
     public static void lockOn(Activity mActivity){
          DevicePolicyManager mDPM;
